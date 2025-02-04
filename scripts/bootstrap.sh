5d7e77855538669b436eccd54c713ed05dc2af96
Cody Watzen <cwatzen@gmail.com>
2025-02-03 21:09:20 -0600

    added bashrc

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,9 +4,19 @@ curl -sS https://starship.rs/install.sh | sh
 
 mkdir -p ~/.config/zsh
 
+mkdir -p ~/.config/functions
+
+mkdir -p ~/.config/scripts
+
 stow zsh -d dotfiles/ -t
 
 stow -d dotfiles/zsh -t ~/dotfiles/zsh/plugins plugins
+
+stow functions -d dotfiles/ -t ~/.config/functions
+
+stow scripts -d dotfiles/ -t ~/.config/scripts/
+
+stow alias -d dotfiles/ -t ~/.config/
 
 # Initialize submodules
 git submodule update --init --recursive
